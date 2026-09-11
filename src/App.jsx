d4303059b3470b7275a715da28abaa5dd7cc2aae
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-11 12:27:04 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { Routes, Route } from "react-router";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
-
+import CountriesPage from "./pages/CountriesPage";
+import BucketListPage from "./pages/BucketListPage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 const App = () => {
   // TODO: replace this with a real route table using react-router.
   // For now the app only shows HomePage. Clicking nav links causes a full
   // page reload (or a 404) because there's no client-side routing yet.
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+    <Routes>
+     <Route path="/" element={<Layout />}>
+    <Route index element={<HomePage />} />
+ <Route path="countries" element={<CountriesPage />} />
+ <Route path="bucket-list" element={<BucketListPage />} />
+ <Route path="about" element={<AboutPage />} />
+ <Route path="*" element={<NotFoundPage />} />
+ </Route>
+ </Routes>
+
+   
+   
   );
 };
-
+<Layout>
+      <HomePage />
+    </Layout>
 export default App;
