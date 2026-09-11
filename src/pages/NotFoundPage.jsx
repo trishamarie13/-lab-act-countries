4c058e3a5d57b10d71ce417e9715884376f2f055
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-11 12:29:03 +0800

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router";
 const NotFoundPage = () => {
   return (
-    <div class="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center">
-        <div class="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50">
+    <div className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <div className="mx-auto flex h-16 w-16 items-center justify-center rounded-full bg-indigo-50">
           <svg
-            class="h-10 w-10 text-indigo-600"
+            className="h-10 w-10 text-indigo-600"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
@@ -18,28 +19,26 @@ const NotFoundPage = () => {
           </svg>
         </div>
 
-        <p class="mt-6 text-base font-semibold text-indigo-600">404</p>
+        <p className="mt-6 text-base font-semibold text-indigo-600">404</p>
         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Page not found
         </h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">
+        <p className="mt-6 text-base leading-7 text-gray-600">
           Sorry, we couldn't find the page you're looking for. It might have
           been moved or deleted.
         </p>
 
-        <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link to="#"
             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a
-            href="#"
+          </Link>
+          <Link to="#"
             class="text-sm font-semibold text-gray-900 hover:text-indigo-600"
           >
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
