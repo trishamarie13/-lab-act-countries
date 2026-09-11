00f80a5bda4ca092742d59ae4d9d406eb1e5cc2a
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-11 12:28:47 +0800

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router";
 const HomePage = () => {
   return (
     <div className="max-w-2xl mx-auto text-center py-10 space-y-4">
       <div className="text-6xl">🌍</div>
       <h1 className="text-4xl font-bold">Countries of the World</h1>
       <p>Browse the world's countries and build your travel bucket list.</p>
-      <a href="/countries" className="btn btn-primary">
+      <Link to="/countries" className="btn btn-primary">
         Browse Countries
-      </a>
+      </Link>
     </div>
   );
 };
