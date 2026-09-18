e54918dab728e5e52f36613d99444eadf7473f7f
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-18 12:16:00 +0800

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import COUNTRIES from "../data/countries";
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleSurpriseMe = () => {
+ const random = COUNTRIES[Math.floor(Math.random() * COUNTRIES.length)];
+ navigate(`/countries/${random.code}`);
+ };
+
   return (
     <div className="max-w-2xl mx-auto text-center py-10 space-y-4">
-      <div className="text-6xl">🌍</div>
       <h1 className="text-4xl font-bold">Countries of the World</h1>
       <p>Browse the world's countries and build your travel bucket list.</p>
-      <Link to="/countries" className="btn btn-primary">
-        Browse Countries
-      </Link>
+     <div className="flex gap-2 justify-center">
+       <Link to={"/countries"} className="btn btn-primary">
+         Browse Countries
+        </Link>
+        <button className="btn btn-outline" onClick={handleSurpriseMe}>
+ Surprise Me
+        </button>
     </div>
+  </div>
+
   );
 };
 
