794012576bd54d986e72241ca0234c89abb796c1
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-18 12:12:09 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,27 @@ import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import CountryDetailPage from "./pages/CountryDetailPage";
 const App = () => {
   // TODO: replace this with a real route table using react-router.
   // For now the app only shows HomePage. Clicking nav links causes a full
   // page reload (or a 404) because there's no client-side routing yet.
   return (
     <Routes>
-     <Route path="/" element={<Layout />}>
-    <Route index element={<HomePage />} />
- <Route path="countries" element={<CountriesPage />} />
- <Route path="bucket-list" element={<BucketListPage />} />
- <Route path="about" element={<AboutPage />} />
- <Route path="*" element={<NotFoundPage />} />
- </Route>
- </Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        
+        <Route path="countries">
+          <Route index element={<CountriesPage />} />
+          <Route path=":countryCode" element={<CountryDetailPage />} />
+        </Route>
 
-   
-   
+        <Route path="bucket-list" element={<BucketListPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  
   );
 };
 <Layout>
